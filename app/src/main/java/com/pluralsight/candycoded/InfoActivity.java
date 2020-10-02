fa2ae2896523e87141ba5ec3f9f3c925ebc5528b
Ruchy27Groenewaldt <69246586+Ruchy27Groenewaldt@users.noreply.github.com>
2020-10-02 13:34:52 +0200

    This is a online candy store application
    The application basically acts a broachure and it shows all the products that is available in the store.

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -30,7 +30,7 @@ public class InfoActivity extends AppCompatActivity {
     // ***
 
     public void createMapIntent(View view) {
-        Uri uriAddress = Uri.parse("geo:0,0?q=618 E South St Orlando, FL 32801");
+        Uri uriAddress = Uri.parse("geo:0,0?q=37 Park Road, Mariental");
         Intent mapIntent = new Intent(Intent.ACTION_VIEW, uriAddress);
         mapIntent.setPackage("com.google.android.apps.maps");
 
@@ -47,7 +47,7 @@ public class InfoActivity extends AppCompatActivity {
     // ***
     public void createPhoneIntent(View view){
         Intent phoneIntent= new Intent(Intent.ACTION_DIAL);
-        phoneIntent.setData(Uri.parse("tel:0123456789"));
+        phoneIntent.setData(Uri.parse("tel: 061 234 4568"));
         startActivity(phoneIntent);
 
     }
